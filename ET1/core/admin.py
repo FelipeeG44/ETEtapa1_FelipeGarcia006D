caa17bd4c4082f6eed4b04f271fb5a2b6e2bcce4
unknown <f.garcia@duocuc.cl>
2021-07-06 04:03:01 -0400

    v2

--- ET1/core/admin.py
+++ ET1/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Colaboradores
+
+admin.site.register(Colaboradores)
--- ET1/core/models.py
+++ ET1/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Colaboradores(models.Model):
+    idColaboradores= models.CharField(max_length=10 ,primary_key=True ,verbose_name="RUT Colaborador")
+    nomColaboradores= models.CharField(max_length=30, verbose_name="Nombre Colaborador")
+    telefono=models.IntegerField(verbose_name="Telefono")
+    direccion=models.CharField(max_length=30,verbose_name="Dirección")
+    email=models.CharField(max_length=30,verbose_name="Email")
+    pais=models.CharField( max_length=20,verbose_name="país")
+
+    def __str__(self):
+        return self.idColaboradores
